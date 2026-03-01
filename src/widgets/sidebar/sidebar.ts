a014e776e016afd34e8b699f2bfbccb486ee84b2
Danila Kovalev <kovallll.e@gmail.com>
2026-03-01 20:27:55 +0300

    fix styles

--- src/widgets/sidebar/sidebar.ts
+++ src/widgets/sidebar/sidebar.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { SidebarMenuComponent } from './menu/ui/menu';
 import { AccountComponent } from '@/entities/account/account';
+import { LogoutButtonComponent } from '@/entities/logout-button/logout-button';
 import { AppLogoComponent } from '@/shared';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SidebarMenuComponent, MatIconModule, AccountComponent, AppLogoComponent],
+  imports: [
+    SidebarMenuComponent,
+    MatIconModule,
+    AccountComponent,
+    LogoutButtonComponent,
+    AppLogoComponent,
+  ],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss'],
 })
